48bff555c495b33612bbacfa261b93612d504f43
mpirunthapan <mpirunthapan@gmail.com>
2025-10-02 23:43:06 +0530

    home page finished

--- app/routes/events.tsx
+++ app/routes/events.tsx
@@ -1,4 +1,3 @@
-import { i } from 'node_modules/@react-router/dev/dist/routes-CZR-bKRt'
 import React from 'react'
 import Navbar from '~/components/main/Navbar'
 import Footer from '~/components/main/Footer'
